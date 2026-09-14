2096c88bd904c243373543e6425f90393d8cdba9
zat abdillah <zatabdillahsmkitsi@gmail.com>
2026-09-14 20:57:49 +0700

    kasir sederhana-done

---
+++ task 1,2.py
@@ -0,0 +1,9 @@
+nama=input("Nama barang: ")
+harga=int(input("Harga sewa: "))
+hari=int(input("Waktu sewa: "))
+total=harga*hari
+
+print("Nama barang: ", nama)
+print("Harga Sewa: ", harga)
+print("tenggat waktu: ", hari)
+print("Total harga: ", total)
---
+++ task 1.py
@@ -0,0 +1,9 @@
+nama=input ("Nama barang: ")
+harga=int(input("Harga barang: "))
+jumlah=int(input("Jumlah pembelian: "))
+total=harga*jumlah
+
+print("Nama barang: ", nama)
+print("Harga barang: ", harga)
+print("Jumlah pembelian: ", jumlah)
+print("Total harga: ", total)
